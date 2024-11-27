c2561ec070fc5aed01e530a3d9979d590de3858e
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-11-27 16:57:38 +0100

    add:  header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 
+import PageHeader from "./components/Header/Header.jsx"
+
 function App() {
   
 
   return (
     <>
-      <h1>Il mio blog</h1> 
-    </>
+      <PageHeader />
+
+      {/*
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="section-blog"> 
+              <h1>il mio blog</h1>
+              <div className="card">card-box
+                <div className="card-image">
+                  img
+                </div>
+                <div className="card-content">
+                  description
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+       */}
+  </>
   )
 }
 
