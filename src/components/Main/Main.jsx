2f3c9e0cb699913e331c6f3dbcb4cb2a13f51af7
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-11-27 17:52:04 +0100

    add: main component and card top content

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,28 @@
+
+
+export default function Main() {
+
+    return (
+
+        <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="section-blog"> 
+              <div className="card">
+                <div className="card-image">
+                  <div className="card-post">600 x 400</div>
+                </div>
+                <div className="card-content">
+                  <div className="card-title">Titolo del post</div>
+                  <div className="card-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure fuga non itaque ipsum, eaque consectetur!
+                  </div>
+                  <button className="card-btn">leggi di più</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>   
+    )
+
+}
