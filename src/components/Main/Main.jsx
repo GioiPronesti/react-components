7dc824ff78dee097063751ae92fa7149dcaafb85
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-11-27 19:21:22 +0100

    fix: add change of layout structure

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,24 +5,26 @@ export default function Main() {
     return (
 
         <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="section-blog"> 
-              <div className="card">
-                <div className="card-image">
-                  <div className="card-post">600 x 400</div>
+            <div className="row">
+                <div className="col-12">
+                    <section className="section-blog">
+                        
+                        <div className="card">
+                            <div className="card-image">
+                                <div className="card-post">600 x 400</div>
+                            </div>
+                            <div className="card-content">
+                                <h2 className="card-title">Titolo del post</h2>
+                                <p className="card-description">Lorem ipsum dolor sit, amet  consectetur adipisicing elit. Iure fuga non itaque ipsum, eaque consectetur!
+                                </p>
+                            </div>
+                            
+                        </div>
+                    </section>   
                 </div>
-                <div className="card-content">
-                  <div className="card-title">Titolo del post</div>
-                  <div className="card-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure fuga non itaque ipsum, eaque consectetur!
-                  </div>
-                  <button className="card-btn">leggi di più</button>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>   
+          
     )
 
 }
